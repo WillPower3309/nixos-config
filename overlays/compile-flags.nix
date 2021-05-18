78ce427c5ba47ea68f346c1bb4a34c82cbc4709b
Will McKinnon <contact@willmckinnon.com>
2021-05-18 00:28:14 -0400

    compile-flags now inherits impureUseNativeOptimizations

--- overlays/compile-flags.nix
+++ overlays/compile-flags.nix
@@ -1,8 +1,8 @@
 final: prev:
 {
   stdenv = prev.stdenvAdapters.addAttrsToDerivation {
-    NIX_CFLAGS_COMPILE = "-pipe -march=native -O2";
+    #NIX_CFLAGS_COMPILE = "-pipe -O2";
     #NIX_LDFLAGS = "";
-    makeFlags = "-j5";
-  } prev.stdenv;
+    #makeFlags = "-j5";
+  } (prev.impureUseNativeOptimizations prev.stdenv);
 }

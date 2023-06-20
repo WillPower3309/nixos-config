26038107737338f74996d2c7af97ba534f764026
William McKinnon <contact@willmckinnon.com>
2023-06-19 23:17:51 -0400

    added ssh config

--- home/ssh-client.nix
+++ home/ssh-client.nix
@@ -3,6 +3,18 @@
 {
   programs.ssh = {
     enable = true;
+
+    matchBlocks = {
+      "server*" = {
+        hostname = "10.27.27.3";
+        user = "root";
+        identityFile = "~/.ssh/server";
+      };
+
+      "server-boot" = {
+        port = 2222;
+      };
+    };
   };
 
   home.persistence."/nix/persist/home/will".directories = [ ".ssh" ];

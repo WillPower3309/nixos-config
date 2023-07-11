fe6bff30312c2049195a0438dd8eaf8e55cc9695
William McKinnon <contact@willmckinnon.com>
2023-07-11 18:18:14 -0400

    added timeout 0 to bootloader

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -8,6 +8,8 @@
         editor = false; # true allows gaining root access by passing init=/bin/sh as a kernel parameter
         consoleMode = "max";
       };
+
+      timeout = 0;
     };
   };
 }

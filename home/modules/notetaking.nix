44b89f165633ad1ec7e22ec2f0db13f2935ac6a2
William McKinnon <contact@willmckinnon.com>
2023-11-01 02:08:04 -0400

    updated notetaking config

--- home/modules/notetaking.nix
+++ home/modules/notetaking.nix
@@ -3,6 +3,11 @@
 {
   home = {
     packages = [ pkgs.obsidian ];
-    persistence."/nix/persist/home/will".directories = [ "Notes" ];
+
+    persistence."/nix/persist/home/will".directories = [
+      "notes"
+      # TODO: replace below with declarative config
+      ".config/obsidian"
+    ];
   };
 }

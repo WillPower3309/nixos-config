925c8c4361bec63d9254f9dc27ab156c88c2211d
William McKinnon <contact@willmckinnon.com>
2024-02-26 02:49:42 -0500

    moved config comment to be nix comment

--- modules/screen-record.nix
+++ modules/screen-record.nix
@@ -16,10 +16,10 @@
       "snd-aloop"
     ];
 
+    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+    # https://github.com/umlaeute/v4l2loopback
     extraModprobeConfig = ''
-      # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
-      # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
-      # https://github.com/umlaeute/v4l2loopback
       options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
     '';
   };

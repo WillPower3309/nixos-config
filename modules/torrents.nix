f9f527b9a5feb6450a8fd86bab1e929a1dee0327
William McKinnon <contact@willmckinnon.com>
2024-02-26 02:50:37 -0500

    added torrent module

---
+++ modules/torrents.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  services.transmission = {
+    enable = true;
+    openRPCPort = true;
+    settings = {
+      download-dir = "/persist/transmission/download"; # TODO
+      incomplete-dir = "/persist/transmission/incomplete"; # TODO
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1,10.27.27.5";
+    };
+  };
+}
+

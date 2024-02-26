34d20898ee81862a67f895146d52ef8a754d48e8
William McKinnon <contact@willmckinnon.com>
2024-02-26 02:50:37 -0500

    added torrent module

--- modules/torrents.nix
+++ modules/torrents.nix
@@ -8,7 +8,7 @@
       download-dir = "/persist/transmission/download"; # TODO
       incomplete-dir = "/persist/transmission/incomplete"; # TODO
       rpc-bind-address = "0.0.0.0";
-      rpc-whitelist = "127.0.0.1,10.27.27.5";
+      rpc-whitelist = "127.0.0.1,10.27.27.5,192.168.100.*"; # allow desktop and nebula devices to connect
     };
   };
 }

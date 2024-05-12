0ebb6cb14bc2234398b7ec0e670aea86525d4466
William McKinnon <contact@willmckinnon.com>
2024-05-12 15:26:14 -0400

    updated ags config to include notifications

--- home/modules/config/ags/config.js
+++ home/modules/config/ags/config.js
@@ -1,7 +1,9 @@
 import { applauncher } from "./widgets/app-launcher.js";
+import { NotificationPopups } from "./widgets/notification-popups.js"
 
 App.config({
     windows: [
         applauncher,
+        NotificationPopups(),
     ],
 })
--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -7,7 +7,6 @@ in
 {
   home.packages = with pkgs; [
     swaybg
-    mako
     nwg-launchers
     autotiling
     slurp

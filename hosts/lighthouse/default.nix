68380a590dcf0c4705343faeb812b3b55b62ca0a
William McKinnon <contact@willmckinnon.com>
2024-09-11 02:05:19 -0400

    added fail2ban

--- hosts/lighthouse/default.nix
+++ hosts/lighthouse/default.nix
@@ -7,16 +7,6 @@
     ../../modules/nix.nix
   ];
 
-  # the ssh keys are set up on the digitalocean web ui
-  services.openssh = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-    };
-  };
-
   # TODO: cr in nixpkgs to add option for setting nebula user / group
   age.secrets = {
     nebulaCaCert = {
@@ -37,13 +27,36 @@
   };
   networking.firewall.allowedUDPPorts = [ 4242 ];
 
-  services.nebula.networks.home = {
-    enable = true;
-    isLighthouse = true;
-    isRelay = true;
-    cert = config.age.secrets.lighthouseNebulaCert.path; # lighthouse.crt
-    key = config.age.secrets.lighthouseNebulaKey.path; # lighthouse.key
-    ca = config.age.secrets.nebulaCaCert.path; # ca.crt
+  services = {
+     # the ssh keys are set up on the digitalocean web ui
+    openssh = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
+    };
+
+    nebula.networks.home = {
+      enable = true;
+      isLighthouse = true;
+      isRelay = true;
+      cert = config.age.secrets.lighthouseNebulaCert.path; # lighthouse.crt
+      key = config.age.secrets.lighthouseNebulaKey.path; # lighthouse.key
+      ca = config.age.secrets.nebulaCaCert.path; # ca.crt
+    };
+
+    fail2ban = {
+      enable = true;
+      maxretry = 2;
+      bantime = "24h";
+      bantime-increment = {
+        enable = true;
+        multipliers = "1 2 4 8 16 32 64";
+        overalljails = true; # Calculate the bantime based on all the violations
+      };
+    };
   };
 
   system.stateVersion = "23.05";

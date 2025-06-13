ce38f679a87ffe857868e5aef73cc87ba8dff6b1
William McKinnon <contact@willmckinnon.com>
2025-06-13 17:24:13 -0400

    updated web browsers

--- home/modules/web-browsers.nix
+++ home/modules/web-browsers.nix
@@ -4,12 +4,26 @@
   programs.chromium = {
     enable = true;
     package = pkgs.ungoogled-chromium;
-    # TODO: extensions
+    # TODO: these don't seem to work
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+      { id = "oboonakemofpalcgghocfoadofidjkkk"; } # keepassxc-browser
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
+    ];
   };
 
   programs.qutebrowser = {
     enable = true;
   };
 
-  home.persistence."/nix/persist/home/will".directories = [ ".config/chromium" ];
+  home = {
+    persistence."/nix/persist/home/will" = {
+      files = [ ".config/chromium/Default/Preferences" ];
+    };
+    file.".config/chromium/First Run" = {
+      text = "";
+      recursive = true;
+    };
+  };
 }
+

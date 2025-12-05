5a9a7988946e36a0866416498a7c7b80debf3d3e
William McKinnon <contact@willmckinnon.com>
2025-12-04 21:10:00 -0500

    fix: ensure term buffer is hidden with keybind

--- home/modules/vim/config/init.lua
+++ home/modules/vim/config/init.lua
@@ -16,36 +16,27 @@ vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap=true, silent=true })
 -- better find
 vim.keymap.set('n', 'n', 'nzz')
 
--- Term Toggle Keymap
+-- Term toggle key map
+-- TODO: this breaks if a non term win is opened in the window that a term was spawned in
 local term_buf = nil
 local term_win = nil
-vim.keymap.set("n", "<leader>oe", function()
+vim.keymap.set('n', '<leader>oe', function()
     if term_win and vim.api.nvim_win_is_valid(term_win) then
-        vim.cmd("hide")
+        vim.api.nvim_win_close(term_win, true)
     else
-        vim.cmd("botright new")
+        vim.cmd('botright new')
         local new_buf = vim.api.nvim_get_current_buf()
-        vim.cmd("resize " .. 15)
+        vim.cmd('resize ' .. 15)
         if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
-            vim.cmd("buffer " .. term_buf) -- go to terminal buffer
-            vim.cmd("bd " .. new_buf) -- cleanup new buffer
+            vim.cmd('buffer ' .. term_buf) -- go to terminal buffer
+            vim.cmd('bd ' .. new_buf) -- cleanup new buffer
         else
-            vim.cmd("terminal")
+            vim.cmd('terminal')
             term_buf = vim.api.nvim_get_current_buf()
         end
-    vim.cmd.startinsert()
     term_win = vim.api.nvim_get_current_win()
     end
 end)
-
--- built in terminal config
-vim.opt_local.mouse = '' -- TODO: terminal only
-vim.api.nvim_create_autocmd({ 'WinEnter' }, {
-    pattern = term_win,
-    callback = function()
-        vim.cmd.startinsert()
-    end
-})
 
 -- Disable unused builtins and language provider support (lua and vimscript plugins only, LSP handles the rest)
 vim.g.loaded_perl_provider = 0

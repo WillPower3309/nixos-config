3683b1fd2e601cdb05a59a0f651cfe0cc78bb039
William McKinnon <contact@willmckinnon.com>
2026-02-28 23:28:19 -0500

    removed programs.adb

--- modules/android-dev.nix
+++ modules/android-dev.nix
@@ -7,12 +7,12 @@
   };
 
   # need home module too
-  programs.adb.enable = true;
-  users.users.will.extraGroups = [ "adbusers" ];
+  users.users.will.extraGroups = [ "adbusers" ]; # TODO: still needed?
 
   # needed here due to https://discourse.nixos.org/t/too-many-open-files-when-the-gradle-cache-is-persisted-via-impermanence/51560/6
-  environment.persistence."/nix/persist".users.will.directories = [
-    ".gradle"
-  ];
+  environment = {
+    systemPackages = [ pkgs.android-tools ];
+    persistence."/nix/persist".users.will.directories = [ ".gradle" ];
+  };
 }
 

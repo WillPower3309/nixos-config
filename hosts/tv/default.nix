b139f912a02c8c1982e66100e84ca43f673a9d12
William McKinnon <contact@willmckinnon.com>
2026-04-10 12:03:48 -0400

    update tv config

--- hosts/tv/default.nix
+++ hosts/tv/default.nix
@@ -1,3 +1,8 @@
+# TODO:
+# - hardware acceleration for interstellar
+# - wake on USB
+# - proper CEC turn on / off and switch back from switch 2
+
 { config, pkgs, lib, inputs, ... }:
 
 let
@@ -13,13 +18,33 @@ in
     ../../modules/nix.nix
   ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" ];
+  boot = {
+    initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" ];
+    kernelParams = [
+      "vt.global_cursor_default=0"  # disable VT switching flicker
+      "i915.fastboot=1" # prevent unnecessary mode-sets that cause flickering
+      "i915.enable_psr=0"  # disable Panel Self Refresh (causes flicker on linux)
+      "video=efifb:off" # disable the EFI framebuffer to let the driver take over fully
+    ];
+  };
+
+  services.udev.packages = [ pkgs.game-devices-udev-rules ];
+
   hardware = {
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     enableAllFirmware = true;
-    graphics.enable = true;
     bluetooth.enable = false;
+
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        libva
+        libva-utils
+      ];
+    };
   };
+  powerManagement.cpuFreqGovernor = "powersave";
 
   services = {
     pulseaudio.enable = false;
@@ -31,6 +56,8 @@ in
   networking = {
     hostName = "tv";
     wireless.enable = false;
+    interfaces.eno1.wakeOnLan.enable = true;
+    firewall.allowedUDPPorts = [ 9 ]; # wake on LAN port
   };
 
   users = {
@@ -41,7 +68,7 @@ in
       };
       kodi = {
         isNormalUser = true;
-        extraGroups = [ "video" "audio" "input" ]; # TODO: do I need any more groups? https://forums.raspberrypi.com/viewtopic.php?t=251645
+        extraGroups = [ "video" "audio" "input" "render" "dialout" ]; # dialout needed for CEC serial device
       };
     };
     mutableUsers = false;
@@ -60,56 +87,54 @@ in
     }];
   };
 
-  environment.systemPackages = with pkgs; [ libcec polkit ];
+  # script to start moonlight on tty2
+  systemd.services."getty@tty2".enable = false; # prevent login prompts from interfering
+  environment.systemPackages = let
+    moonlight-kodi-wrapper = pkgs.writeShellScriptBin "launch-moonlight" ''
+      KODI_PID=$(pidof kodi.bin)
+      if [ -n "$KODI_PID" ]; then
+        kill -STOP "$KODI_PID"
+      fi
 
-  # allow access to pulse eight cec device for video group
-  services.udev.extraRules = ''
-    KERNEL=="ttyACM0", GROUP="video", MODE="0660", TAG+="systemd", ENV{SYSTEMD_ALIAS}="/dev/vchiq"
-  '';
+      export QT_QPA_PLATFORM=linuxfb
+      export MOONLIGHT_HWDEC=vaapi
+      export LIBVA_DRIVER_NAME=iHD
+
+      # Run Moonlight on tty2 using openvt
+      openvt -c 2 -s -f -- ${pkgs.moonlight-qt}/bin/moonlight
+
+      # When it exits, return to Kodi on tty1
+      chvt 1
+      if [ -n "$KODI_PID" ]; then
+        kill -CONT "$KODI_PID"
+      fi
+    '';
+  in [ moonlight-kodi-wrapper pkgs.libcec pkgs.polkit ];
+  environment.variables = {
+    ML_AUDIO = "sdl";
+    SDL_AUDIODRIVER = "alsa";
+  };
+  security.wrappers.chvt = {
+    source = "${pkgs.kbd}/bin/chvt";
+    owner = "root";
+    group = "root";
+    setuid = true;
+  };
 
   # TODO: custom plugins https://discourse.nixos.org/t/how-to-add-custom-kodi-plugins-yet-another-how-to-use-a-custom-derivation-in-my-flake-post/46238
     # remove nixpkgs.config.kodi.enableAdvancedLauncher = true
     # https://github.com/jurialmunkey/skin.arctic.fuse.3
     # https://github.com/croneter/PlexKodiConnect
   nixpkgs.config.kodi.enableAdvancedLauncher = true;
+
   services.getty.autologinUser = "kodi";
   environment.loginShellInit = let kodi-package = pkgs.kodi-gbm.withPackages(kodiPkgs: with kodiPkgs; [
     inputstream-adaptive
     joystick
-    plex-for-kodi
-    youtube
   ]);
   in ''
     [[ "$(tty)" = "/dev/tty1" ]] && ${kodi-package}/bin/kodi-standalone
   '';
-
-  # TODO: hardening
-  systemd = {
-    sockets.cec-client = {
-      after = [ "dev-vchiq.device" ];
-      bindsTo = [ "dev-vchiq.device" ];
-      wantedBy = [ "sockets.target" ];
-      socketConfig = {
-        ListenFIFO = "/run/cec.fifo";
-        SocketGroup = "video";
-        SocketMode = "0660";
-      };
-    };
-    services.cec-client = {
-      after = [ "dev-vchiq.device" ];
-      bindsTo = [ "dev-vchiq.device" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = ''${pkgs.libcec}/bin/cec-client -d 1'';
-        ExecStop = ''/bin/sh -c "echo q > /run/cec.fifo"'';
-        StandardInput = "socket";
-        StandardOutput = "journal";
-        Restart="no";
-      };
-    };
-  };
-
-  users.extraUsers.kodi.isNormalUser = true;
 
   #home-manager.users.kodi = {
   #  programs.home-manager.enable = true;
@@ -124,7 +149,7 @@ in
   #        source = "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so";
   #        target = ".kodi/cdm/libwidevinecdm.so";
   #      };
-  #      widevine-manifest= {
+  #      widevine-manifest = {
   #        source = "${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm/manifest.json";
   #        target = ".kodi/cdm/manifest.json";
   #      };
@@ -138,6 +163,7 @@ in
       directories = [
         "/var/log"
         "/var/lib/nixos"
+        # TODO: add below to HM
         { directory = "/home/kodi/.kodi"; user = "kodi"; group = "users"; }
         { directory = "/home/kodi/.cache"; user = "kodi"; group = "users"; }
       ];

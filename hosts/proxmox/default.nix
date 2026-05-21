5d3f43e84a1e58daed9f96e5e9d5b3b1193831b8
William McKinnon <contact@willmckinnon.com>
2026-05-20 20:56:14 -0400

    fixed some issues with proxmox

--- hosts/proxmox/default.nix
+++ hosts/proxmox/default.nix
@@ -3,40 +3,55 @@
 let
   authorizedKey = (builtins.readFile ../../home/id_ed25519.pub);
   hostKeyPath = /etc/ssh/ssh_host_ed25519_key;
-  ipAddress = "10.1.10.10";
+  ipAddress = "10.1.10.3";
   rj45Interface0 = "eth0";
   rj45Interface1 = "eth1";
-  sfpInterface0 = "eth2";
-  sfpInterface1 = "eth3";
-  bridgeInterface = "vmbr0";
+  sfpInterface0 = "sfp0";
+  sfpInterface1 = "sfp1";
 
+  # set a number of network interface VFs equal to the number of VMs + 2
+  # (one for router WAN and one for the PVE host itself)
+  numNetVfs = lib.pipe ./vms [
+    builtins.readDir
+    builtins.attrNames
+    builtins.length
+    (count: count + 2)
+  ];
   numGpuVfs = 2; # only need for plex and immich vms
 
 in
 {
   imports = [
     inputs.agenix.nixosModules.default
-    inputs.impermanence.nixosModules.impermanence
     inputs.proxmox-nixos.nixosModules.proxmox-ve
     ./disks.nix
-    ../../modules/nix.nix
   ];
 
-  nixpkgs.overlays = [
-    inputs.proxmox-nixos.overlays.x86_64-linux
-  ];
+  nixpkgs.overlays = [ inputs.proxmox-nixos.overlays.x86_64-linux ];
 
-  services = {
-    # 38:05:28:31:AD / rj45Interface1 is the intel AMT ethernet port (uses the I226-LM controller)
-    udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:ac", NAME="${rj45Interface0}"
-      ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:ad", NAME="${rj45Interface1}"
-      ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:aa", NAME="${sfpInterface0}"
-      ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:ab", NAME="${sfpInterface1}"
-    '';
-  };
+  # 1. Match by MAC, assign stable kernel-style name:
+  #   38:05:28:31:AD / rj45Interface0: corosync
+  #   38:05:28:31:AD / rj45Interface1: intel AMT ethernet port (as it uses the I226-LM controller)
+  #   38:05:28:31:AD / sfpInterface0: SR-IOV - used for host and VMs
+  #   38:05:28:31:AD / sfpInterface1: unused
+  # 2. Trigger SR-IOV creation once the SFP driver registers sfpInterface0
+  # 3. Disable "spoof checking" and enables "trust" on VF 0 so the host can handle advanced network stacks
+  # 4. Enforce the embedded hardware switch to allow local VF-to-VF looping
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:ac", NAME="${rj45Interface0}"
+    ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:ad", NAME="${rj45Interface1}"
 
-  networking = {
+    ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:aa", NAME="${sfpInterface0}", RUN+="${pkgs.bash}/bin/bash -c '\
+      echo ${numNetVfs} > /sys/class/net/%k/device/sriov_numvfs && \
+      ${pkgs.iproute2}/bin/ip link set dev %k vf 0 trust on spoofchk off && \
+      ${pkgs.udev}/bin/udevadm trigger --attr-match=subsystem=net'"
+    ACTION=="add", SUBSYSTEM=="net", KERNEL=="${sfpInterface0}v0", RUN+="${pkgs.iproute2}/bin/bridge link set dev %k hairpin on"
+
+    ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="38:05:25:31:58:ab", NAME="${sfpInterface1}"
+  '';
+
+  # use the first virtual function as the host's interface
+  networking = let hostInterface = "${sfpInterface0}v0"; in {
     hostName = "proxmox";
     usePredictableInterfaceNames = false; # set interface names via services.udev.extraRules above
     useDHCP = false;
@@ -44,40 +59,50 @@ in
     networkmanager.enable = lib.mkForce false;
     useNetworkd = true;
 
-    defaultGateway = {
-      address = "10.1.10.1";
-      interface = rj45Interface0;
-    };
-
-    bridges."${bridgeInterface}".interfaces = [ rj45Interface0 ];
-
-    interfaces = {
-      "${bridgeInterface}".ipv4.addresses = [{
+    # Bind the Host Static IP directly to the first Virtual Function (VF 0)
+    interfaces."${hostInterface}" = {
+      # TODO: use "stable" instead?
+      macAddress = "38:05:25:31:58:af"; # VFs get a randomized mac on boot: set a static mac to be nice to ARP caches
+      ipv4.addresses = [{
         address = ipAddress;
         prefixLength = 24;
       }];
+    };
+
+    # Route host traffic strictly through the dedicated VF
+    defaultGateway = {
+      address = "10.1.10.1"; # TODO: define statically somewhere as this is also needed for router
+      interface = "${hostInterface}";
     };
 
     # TODO: ceph USB4 mesh network: https://fangpenlin.com/posts/2024/01/14/high-speed-usb4-mesh-network/
     # TODO: fallback routing for the ceph mesh network: https://pve.proxmox.com/wiki/Full_Mesh_Network_for_Ceph_Server#Routed_Setup_(with_Fallback)
   };
 
-  # TODO sriov gpu: https://www.michaelstinkerings.org/gpu-virtualization-with-intel-12th-gen-igpu-uhd-730/
-  boot = {
-    kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" ]; # needed for sriov
-    kernelParams = [ "intel_iommu=on" "iommu=pt" "i915.enable_guc=3" "i915.max_vfs=${toString numGpuVfs}" ]; # needed for sriov
-    kernel.sysfs = {
-      #devices."pci0000:00"."0000:00:02.0".sriov_numvfs = numGpuVfs; # set the number of vfs for the igpu
-      # TODO: doesn't work with udev.extrarules
-      # TODO: set by mathing the number of files in vms folder
-      class.net.${sfpInterface0}.device.sriov_numvfs = 7; # set the number of vfs for the nic
+  age.secrets.hashedRootPassword.file = ../../secrets/hashedRootPassword.age;
+
+  users = {
+    users.root = {
+      hashedPasswordFile = config.age.secrets.hashedRootPassword.path;
+      openssh.authorizedKeys.keys = [ authorizedKey ];
     };
+    mutableUsers = false;
+  };
+
+  boot = {
+    # needed for SR-IOV
+    kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" ];
+    kernelParams = [ "intel_iommu=on" "iommu=pt" "i915.enable_guc=3" "i915.max_vfs=${toString numGpuVfs}" ];
+    # TODO sriov gpu: https://www.michaelstinkerings.org/gpu-virtualization-with-intel-12th-gen-igpu-uhd-730/
+    # kernel.sysfs.devices."pci0000:00"."0000:00:02.0".sriov_numvfs = numGpuVfs; # set the number of vfs for the igpu
+
     loader.systemd-boot = {
       enable = true;
       editor = false; # true allows gaining root access by passing init=/bin/sh as a kernel parameter
     };
 
     initrd = {
+      # TODO: does this still work for SFP port
       kernelModules = [ "igc" ]; # intel ethernet controller
 
       network = {
@@ -94,16 +119,6 @@ in
 
   hardware.enableAllFirmware = true;
 
-  age.secrets.hashedRootPassword.file = ../../secrets/hashedRootPassword.age;
-
-  users = {
-    users.root = {
-      hashedPasswordFile = config.age.secrets.hashedRootPassword.path;
-      openssh.authorizedKeys.keys = [ authorizedKey ];
-    };
-    mutableUsers = false;
-  };
-
   services.openssh = {
     enable = true;
     openFirewall = true;
@@ -117,19 +132,10 @@ in
     }];
   };
 
-  # Set your time zone.
-  time.timeZone = "America/Toronto";
+  age.secrets.proxmoxRsaPrivateKey.file = ../../secrets/proxmoxRsaPrivateKey.age;
 
   environment = {
-    persistence."/nix/persist" = {
-      hideMounts = true;
-      directories = [
-        "/var/log"
-        "/var/lib/nixos"
-        "/var/lib/pve-cluster"
-      ];
-      files = [ (toString hostKeyPath) ];
-    };
+    persistence."/nix/persist".directories = [ "/var/lib/pve-cluster" ];
 
     etc = {
       "ssh/ssh_host_ed25519_key.pub".source = ./ssh_host_ed25519_key.pub;
@@ -138,15 +144,11 @@ in
       "ssh/ssh_host_rsa_key".source = config.age.secrets.proxmoxRsaPrivateKey.path;
     };
   };
-  age.secrets.proxmoxRsaPrivateKey.file = ../../secrets/proxmoxRsaPrivateKey.age;
 
   services.proxmox-ve = {
     enable = true;
     openFirewall = true;
     ipAddress = ipAddress;
-    bridges = [ bridgeInterface ];
   };
-
-  system.stateVersion = config.system.nixos.release;
 }
 

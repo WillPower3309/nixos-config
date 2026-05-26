628a7d06393a352dfdbe92ec39eb4b563204d8a8
William McKinnon <contact@willmckinnon.com>
2026-05-26 03:16:05 -0400

    added deploy-rs config

---
+++ modules/collectors.nix
@@ -0,0 +1,19 @@
+{ inputs, config, lib, ... }:
+
+{
+  flake.deploy.nodes = lib.mapAttrs (name: cfg: {
+    hostname = cfg.config.networking.fqdn;
+    profiles.system = {
+      user = "root";
+      sshUser = "root";
+      sshOpts = [ "-p" (builtins.toString (builtins.elemAt cfg.config.services.openssh.ports 0)) ];
+      path = lib.pipe cfg [
+        (c: c.pkgs.targetPlatform.system)
+        (arch: inputs.deploy-rs.lib.${arch}.activate.nixos or (throw "Unsupported architecture: ${arch}"))
+        (activate: activate cfg)
+      ];
+    };
+  }) config.flake.nixosConfigurations;
+
+  flake.checks = builtins.mapAttrs (_: deployLib: deployLib.deployChecks config.flake.deploy) inputs.deploy-rs.lib;
+}

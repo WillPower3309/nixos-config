8be415522352c8f342a325f2655b17d10cf54c54
William McKinnon <contact@willmckinnon.com>
2026-05-27 01:45:50 -0400

    made secrets.nix dynamic

---
+++ secrets.nix
@@ -0,0 +1,61 @@
+let
+  flakeConfig = builtins.getFlake (toString ./.);
+  nixosConfigs = flakeConfig.nixosConfigurations;
+  homeConfigs = flakeConfig.homeConfigurations;
+
+  userWill = builtins.readFile ./modules/features/ssh-client/id_ed25519.pub;
+  editors = [ userWill ];
+
+  stripContext = builtins.unsafeDiscardStringContext;
+
+  secretsFromHost = hostName:
+    let
+      hostConfig = builtins.getAttr hostName nixosConfigs;
+      hasAgeSecrets = builtins.hasAttr "age" hostConfig.config
+        && builtins.hasAttr "secrets" hostConfig.config.age;
+    in
+    if hasAgeSecrets then
+      builtins.map (secretName:
+        let
+          secretConfig = builtins.getAttr secretName hostConfig.config.age.secrets;
+          file = "./secrets/${builtins.baseNameOf (stripContext (toString secretConfig.file))}";
+        in
+        {
+          inherit file;
+          key = builtins.readFile ./modules/hosts/${hostName}/ssh_host_ed25519_key.pub;
+        }
+      ) (builtins.attrNames hostConfig.config.age.secrets)
+    else
+      [ ];
+
+  secretsFromHome = homeName:
+    let
+      homeConfig = builtins.getAttr homeName homeConfigs;
+      hasAgeSecrets = builtins.hasAttr "age" homeConfig.config
+        && builtins.hasAttr "secrets" homeConfig.config.age;
+    in
+    if hasAgeSecrets then
+      builtins.map (secretName:
+        let
+          secretConfig = builtins.getAttr secretName homeConfig.config.age.secrets;
+          file = "./secrets/${builtins.baseNameOf (stripContext (toString secretConfig.file))}";
+        in
+        {
+          inherit file;
+        }
+      ) (builtins.attrNames homeConfig.config.age.secrets)
+    else
+      [ ];
+
+  allSecrets = builtins.concatLists [
+    (builtins.concatLists (builtins.map secretsFromHost (builtins.attrNames nixosConfigs)))
+    (builtins.concatLists (builtins.map secretsFromHome (builtins.attrNames homeConfigs)))
+  ];
+
+  groupedSecrets = builtins.groupBy (item: item.file) allSecrets;
+
+in builtins.mapAttrs (fileString: occurrences: {
+  publicKeys = (builtins.filter (k: k != null)
+    (map (occurrence: occurrence.key or null) occurrences)) ++ editors;
+}) groupedSecrets
+

1f1481239ea55a28d4bfa0acc579bbd655156fdf
William McKinnon <contact@willmckinnon.com>
2026-09-24 17:18:11 -0400

    remove indent

--- modules/common/impermanence.nix
+++ modules/common/impermanence.nix
@@ -4,15 +4,13 @@
   flake.modules.nixos.impermanence = { config, ... }: {
     imports = [ inputs.impermanence.nixosModules.impermanence ];
 
-    environment = {
-      persistence.${config.constants.persistentDir} = {
-        hideMounts = true;
-        directories = [
-          "/var/log"
-          "/var/lib/nixos"
-        ];
-        files = [ "/etc/ssh/ssh_host_ed25519_key" ];
-      };
+    environment.persistence.${config.constants.persistentDir} = {
+      hideMounts = true;
+      directories = [
+        "/var/log"
+        "/var/lib/nixos"
+      ];
+      files = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
   };
 }
